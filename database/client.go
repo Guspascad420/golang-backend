52a695fde959f43c7459f7f3257b479bad00db8a
Guspascad420 <Paacad@gmail.com>
2023-06-07 08:44:22 +0700

    Updated mysql datasource url

--- database/client.go
+++ database/client.go
@@ -4,7 +4,6 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
-	"os"
 	"test/models"
 )
 
@@ -12,8 +11,7 @@ var Db *gorm.DB
 var err error
 
 func Connect() {
-	port := os.Getenv("PORT")
-	dsn := "root:rahasia@tcp(0.0.0.0:" + port + ")/projects?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:0he0dFqkGG738S8Eo9gc@tcp(containers-us-west-95.railway.app:6719)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Cannot connect to DB")
