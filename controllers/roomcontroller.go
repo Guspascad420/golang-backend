bd4c91586c6de4cd2a69dafa361c8a05096b83dc
Guspascad420 <Paacad@gmail.com>
2023-06-07 14:52:53 +0700

    Added room route and controller

--- controllers/roomcontroller.go
+++ controllers/roomcontroller.go
@@ -2,9 +2,20 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"test/database"
+	"test/models"
 )
 
 func GetAllRooms(c *gin.Context) {
-	//building := c.Param("building")
-	//rooms := database.Db.Table("").Where("building")
+	building := c.Param("building")
+	var rooms []models.Room
+	record := database.Db.Where("building = ?", building).Find(&rooms)
+
+	if record.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"meta": &models.Meta{false, record.Error.Error()}})
+		c.Abort()
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"meta": &models.Meta{true, "Success"}, "data": rooms})
 }
