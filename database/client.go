04db4815994f2f855506d828c54bf48550109829
Guspascad420 <Paacad@gmail.com>
2023-06-07 08:15:50 +0700

    Updated IP

--- database/client.go
+++ database/client.go
@@ -11,7 +11,7 @@ var Db *gorm.DB
 var err error
 
 func Connect() {
-	dsn := "root:rahasia@tcp(127.0.0.1:3306)/projects?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:rahasia@tcp(0.0.0.0:3306)/projects?charset=utf8mb4&parseTime=True&loc=Local"
 	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Cannot connect to DB")
