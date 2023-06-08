e0d55f726e6f949ec1957dcc046b16b85ed76060
Guspascad420 <Paacad@gmail.com>
2023-06-08 13:17:10 +0700

    Updated model columns

--- models/booking.go
+++ models/booking.go
@@ -4,6 +4,7 @@ type Booking struct {
 	ID     uint   `gorm:"primarykey"`
 	Name   string `json:"name"`
 	Date   string `json:"date"`
+	Time   string `json:"time"`
 	RoomID uint   `json:"room_id"`
 	Room   Room   `json:"room"`
 }
--- models/room.go
+++ models/room.go
@@ -4,8 +4,7 @@ import "gorm.io/gorm"
 
 type Room struct {
 	gorm.Model
-	Number      float32 `json:"number"`
-	Building    string  `json:"building"`
-	IsAvailable bool    `json:"is_available"`
-	Floor       int     `json:"floor"`
+	Number   float32 `json:"number"`
+	Building string  `json:"building"`
+	Floor    int     `json:"floor"`
 }
