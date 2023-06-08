fe649e0a942f925ee3be5d5b87afb1abc77f285e
Guspascad420 <Paacad@gmail.com>
2023-06-08 17:08:40 +0700

    Updated column

--- models/booking.go
+++ models/booking.go
@@ -1,7 +1,9 @@
 package models
 
+import "gorm.io/gorm"
+
 type Booking struct {
-	ID          uint   `gorm:"primarykey"`
+	gorm.Model
 	Name        string `json:"name"`
 	Date        string `json:"date"`
 	Time        string `json:"time"`
