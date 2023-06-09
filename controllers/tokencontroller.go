5f8a0084dd8c1efec9b4cacd2580c4f5c97efc39
Guspascad420 <Paacad@gmail.com>
2023-06-10 00:28:49 +0700

    Added experimental jwt map claims controller and route

--- controllers/tokencontroller.go
+++ controllers/tokencontroller.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 	"test/auth"
 	"test/database"
 	"test/models"
@@ -30,15 +33,55 @@ func GenerateToken(context *gin.Context) {
 	}
 	credentialError := user.CheckPassword(request.Password)
 	if credentialError != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"meta": &models.Meta{false, "Invalid credentials"}})
+		context.JSON(http.StatusUnauthorized, gin.H{"meta": models.Meta{false, "Invalid credentials"}})
 		context.Abort()
 		return
 	}
 	tokenString, err := auth.GenerateJWT(user.Email, user.Username)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"meta": &models.Meta{false, err.Error()}})
+		context.JSON(http.StatusInternalServerError, gin.H{"meta": models.Meta{false, err.Error()}})
 		context.Abort()
 		return
 	}
-	context.JSON(http.StatusOK, gin.H{"meta": &models.Meta{true, "Success"}, "data": &models.LoginResponse{tokenString}})
+	context.JSON(http.StatusOK, gin.H{"meta": models.Meta{true, "Success"}, "data": models.LoginResponse{tokenString}})
+}
+
+func ExtractToken(c *gin.Context) string {
+	token := c.Query("token")
+	if token != "" {
+		return token
+	}
+	bearerToken := c.Request.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1]
+	}
+	return ""
+}
+
+func ExtractEmail(c *gin.Context) (interface{}, error) {
+	tokenString := ExtractToken(c)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("qQH82DVoGXjCQ76pJAt04jeyuR33RTP2"), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		return claims["email"], nil
+	}
+	return 0, nil
+}
+
+func GetEmail(c *gin.Context) {
+	email, err := ExtractEmail(c)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"meta": models.Meta{false, err.Error()}})
+		c.Abort()
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"meta": models.Meta{false, "success"}, "data": email})
 }
