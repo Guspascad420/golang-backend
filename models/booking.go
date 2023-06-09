968a20fd1ff0abb7ed00442b1098af0d78566145
Guspascad420 <Paacad@gmail.com>
2023-06-09 12:24:44 +0700

    Experimental

--- models/booking.go
+++ models/booking.go
@@ -1,16 +1,16 @@
 package models
 
 import (
+	"gorm.io/datatypes"
 	"gorm.io/gorm"
-	"time"
 )
 
 type Booking struct {
 	gorm.Model
-	Name        string    `json:"name"`
-	Date        time.Time `json:"date"`
-	Time        string    `json:"time"`
-	Requirement string    `json:"requirement"`
-	RoomID      uint      `json:"room_id"`
-	Room        Room      `json:"room"`
+	Name        string         `json:"name"`
+	Date        datatypes.Date `json:"date"`
+	Time        string         `json:"time"`
+	Requirement string         `json:"requirement"`
+	RoomID      uint           `json:"room_id"`
+	Room        Room           `json:"room"`
 }
